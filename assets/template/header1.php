2a9422d9ebb515e1bc5211047f40e67d0fda44ff
47_1951060931_trananhphuong <94340711+Phuong050401@users.noreply.github.com>
2021-12-26 14:30:15 +0700

    Add files via upload

---
+++ assets/template/header1.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+
+    <link rel="stylesheet" href="css/styleone.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.1/font/bootstrap-icons.css">
+
+</head>
+
+<body>
+    <div id="header">
+        <div class="h-left">
+            <a href="index.php">
+                <img src="images/image-0724023449749.jpg" alt="" />
+            </a>
+
+            <a href="index.php"><span> Gmail</span></a>
+        </div>
+        <div class="h-right">
+            <a class="button
+           
+           
+           
+           button--low
+          
+           button--mobile-after-hero-only
+          " href="" data-action="for work" data-category="cta" data-label="header">Dành cho công việc</a>
+            <a class="button
+           
+           
+           
+            button--medium
+           
+            button--mobile-before-hero-only
+           " href="login.php" data-action="sign in" data-category="cta" data-label="header">Đăng nhập</a>
+            <a class="button
+           
+           
+           
+            button--mobile-after-hero-only
+           " href="signup.php" data-action="sign in" data-category="cta" data-label="header">Tạo tài khoản</a>
+
+        </div>
+
+    </div>
